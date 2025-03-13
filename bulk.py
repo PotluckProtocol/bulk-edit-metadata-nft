505a2911f6d9a14db75d6a36d34c39e2e963616b
Dracula Presley <pr@potluckprotocol.com>
2025-03-13 12:31:33 -0400

    update
    update

--- bulk.py
+++ bulk.py
@@ -1,9 +1,14 @@
 import json
 
-for x in range(5000): # <--- edit range/total json file
-    with open(f'./metadata/{x}.json', 'r+') as f: # <--- or change to another path
+for x in range(1088):  # Updated range to 1088 files (0-1087)
+    with open(f'C:\\Users\\Admin\\Desktop\\GITHUBREPOS\\ipfs-nft-collection-scrapper\\GMers\\metadata\\{x}.json', 'r+') as f:  # Updated path
         data = json.load(f)
-        data['image'] = f"https://gateway/ipfs/CID/{x}.png" # <--- add `image` value.
-        f.seek(0)        # <--- should reset file position to the beginning.
+        # Update description field
+        data['description'] = "A relic and celebration of FTM. The GMers collab with FUNERAL and DRACULA PRESLEY. GM."
+        # Preserve existing image or add new one if needed
+        # If you want to keep the image line from your example, uncomment the next line
+        # data['image'] = f"https://gateway/ipfs/CID/{x}.png"
+        
+        f.seek(0)        # Reset file position to the beginning
         json.dump(data, f, indent=4)
-        f.truncate()     # remove remaining part
+        f.truncate()     # Remove remaining part
